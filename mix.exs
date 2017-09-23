fefc03d7720297de6d314b5536468f4a9b5a1e8c
LordFeratum <lord.feratum@gmail.com>
2017-09-23 15:40:30 +0200

    modified mix.exs

--- mix.exs
+++ mix.exs
@@ -10,23 +10,19 @@ defmodule Newsapi.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  def package do
+    [ name: :newsapi,
+      files: ["lib", "mix.ex"],
+      maintainers: ["Enrique Piña Monserrat"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/LordFeratum/newsapiex"},
+    ]
+  end
+ 
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
