c1d93faa7156b17ef8940f8e9f7689cb15d6fcbf
LordFeratum <lord.feratum@gmail.com>
2017-09-23 15:54:07 +0200

    modified mix.exs

--- mix.exs
+++ mix.exs
@@ -3,30 +3,34 @@ defmodule Newsapi.Mixfile do
 
   def project do
     [app: :newsapi,
+     description: "Simple client to retrieve data from newsapi.org",
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
     [extra_applications: [:logger]]
   end
 
-  def package do
-    [ name: :newsapi,
-      files: ["lib", "mix.ex"],
+  defp package() do
+    [
+      name: "newsapi",
+      files: ["lib", "mix.exs"],
       maintainers: ["Enrique Piña Monserrat"],
       licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/LordFeratum/newsapiex"},
+      links: %{"Github" => "https://github.com/LordFeratum/newsapiex"}
     ]
   end
  
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
-      {:poison, ">= 1.0.0"}
+      {:poison, ">= 1.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
